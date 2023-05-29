a09f7b2be307405f1a8f4c2e3b5619a241e3761a
Jim Gerth <jim.frederic.gerth@gmail.com>
2023-05-29 11:07:36 +0200

    WIP
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- src/board/board.rs
+++ src/board/board.rs
@@ -1,8 +1,29 @@
 //! Board submodule
 
-use crate::cell::Cell;
+use crate::cell::{Cell, Side};
 
 /// The game board
 pub struct Board {
-    cells: Vec<Cell>,
+    /// The cells 
+    cells: Vec<Option<Cell>>,
+
+    /// The number of cell positions in one row of the board.
+    width: usize,
+}
+
+impl Board {
+    fn get_cell(&self, r: usize, c: usize) -> &Option<Cell> {
+        self.cells.get(r * self.width + c).unwrap()
+    }
+
+    fn get_neighbour(&self, x: usize, y: usize, side: Side) -> Option<Cell> {
+        match side {
+            Side::A =>
+            Side::B =>
+            Side::C =>
+            Side::D =>
+            Side::E =>
+            Side::F =>
+        }
+    }
 }
